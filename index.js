f0e6036e0146f70444bc1ee8692fef052bcf95ae
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-08-23 08:13:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(42 - someValue);
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+  }
+  
+  function distanceTravelledInFeet(someValue, feet) {
+    return Math.abs((feet - someValue) * 264);
+  }
+  
+  function calculatesFarePrice(someValue, feet) {
+    const fare = distanceTravelledInFeet(someValue, feet);
+    if ( fare < 400) {
+      return 0;
+    } else if (fare < 2000 && fare > 400) {
+      return 0.02 * (fare - 400);
+    } else if (fare > 2000 && fare < 2500) {
+      return 25;
+    } else if (fare > 2500) {
+      return "cannot travel that far";
+    }
+  }
